0264071936a7b65cdc9dcde48b44b2a8571d304e
Jesus Torres <jesu_ti91@hotmail.com>
2021-11-13 18:11:48 -0500

    Update DocumentType.java

--- vehicle-api/src/main/java/co/com/jdti/tech/vehicleapi/model/entities/DocumentType.java
+++ vehicle-api/src/main/java/co/com/jdti/tech/vehicleapi/model/entities/DocumentType.java
@@ -1,11 +1,18 @@
 package co.com.jdti.tech.vehicleapi.model.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,4 +31,19 @@ public class DocumentType {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", unique = true, nullable = false, updatable = false)
     private Long id;
+
+    @NotEmpty(message = "La descripcion es obligatoria.")
+    @Column(name = "description", nullable = false, unique = true)
+    private String description;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "documentType")
+    private List<UserEntity> users;
+
+    public void addUser(UserEntity users) {
+        if (this.users == null) {
+            this.users = new ArrayList<>();
+        }
+        this.users.add(users);
+    }
 }
