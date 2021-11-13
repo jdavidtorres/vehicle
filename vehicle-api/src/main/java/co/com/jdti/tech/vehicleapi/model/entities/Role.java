02b3863797f8e751903fb0730d77d0a44b5ebeac
Jesus Torres <jesu_ti91@hotmail.com>
2021-11-13 18:12:37 -0500

    Create Role.java

---
+++ vehicle-api/src/main/java/co/com/jdti/tech/vehicleapi/model/entities/Role.java
@@ -0,0 +1,36 @@
+package co.com.jdti.tech.vehicleapi.model.entities;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "role_name", nullable = false, unique = true)
+    private String roleName;
+
+    @OneToMany(mappedBy = "role")
+    List<UserEntity> users;
+}
