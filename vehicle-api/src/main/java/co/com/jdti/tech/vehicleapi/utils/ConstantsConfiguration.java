46d5196606ddb4462163d7ee8c1e0787f167e0a8
Yisus <jesu_ti91@hotmail.com>
2021-11-16 17:06:35 -0500

    Update ConstantsConfiguration.java

--- vehicle-api/src/main/java/co/com/jdti/tech/vehicleapi/utils/ConstantsConfiguration.java
+++ vehicle-api/src/main/java/co/com/jdti/tech/vehicleapi/utils/ConstantsConfiguration.java
@@ -1,12 +1,12 @@
 package co.com.jdti.tech.vehicleapi.utils;
 
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
 public class ConstantsConfiguration {
 
     public static final String HEADER = "Authorization";
     public static final String PREFIX = "Bearer ";
     public static final int TIME_EXPIRATION_PLUS = 500000;
     public static final String SECRET = "_SECRET_SECRET_SECRET_SECRET_SECRET_SECRET_";
+
+    public static final String MESSAGE = "message";
+    public static final String UNSUCCESSFUL_AUTHENTICATION = "Incorrect authentication";
 }
